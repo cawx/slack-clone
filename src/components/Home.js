ab00fee60a1add884f767f66e7e6afc078aff6d5
Caroly <93351229+cawx@users.noreply.github.com>
2023-03-08 23:07:39 +0200

    Section 1 complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,38 @@
 import React from 'react'
 import Navbar from './Navbar'
 import video1 from '../media/teamd.webm'
+import google from '../media/google.svg'
 
 function Home() {
 
   return (
-    <div className="font-notosans">
-        <section className='section bg-purple'>
+    <div className="font-lato overflow-x-hidden">
+        <section className='section bg-purple pb-20'>
           <Navbar />
-          <div className='container mx-auto flex items-center columns-auto'>
-            <div>
-                <h1 className="text-6xl font-bold text-white">Great teamwork starts with a <span className='text-yellow'>digital HQ</span></h1>
-                <p className="text-xl text-white">With all your people, tools and communication in one place, you can work faster and more flexibly than ever before.</p>
-                <div className='flex space-x-4'>
-                  <button className='p-4 px-8 my-4 text-sm rounded bg-white text-purple font-bold'>SIGN UP WITH EMAIL</button>
-                  <button className='p-4 px-8 my-4 text-sm rounded bg-blue text-white font-bold' id='google-btn'>SIGN UP WITH GOOGLE</button>
-                </div>
-                
-                <p className='text-white font-bold'>Slack is free to try for as long as you’d like</p>
+          <div className='container mx-auto flex flex-col space-x-5 md:flex-row md:space-x-16 items-center columns-auto'>
+            <div className='flex flex-col md:w-4/6 xl:w-6/12 relative'>
+                <h1 className="text-6xl  my-5 font-black text-white text-center md:text-left">Great teamwork starts with a <span className='text-yellow'>digital HQ</span></h1>
+                <p className="w-5/6 text-lg text-white text-center mb-10 font-bold md:text-left">With all your people, tools and communication in one place, you can work faster and more flexibly than ever before.</p>
+                <div className='flex flex-col xl:flex-row xl:space-x-4'>
+                  <button className='p-4 px-8 my-2 text-sm rounded bg-white text-purple font-bold'>SIGN UP WITH EMAIL</button>
+                  <button className='pr-4 my-2 text-sm rounded bg-blue text-white font-bold flex items-center space-x-3' id='google-btn'>
+                    <div className='bg-white p-3 m-1 rounded' >
+                      <img src={google} className='w-5' />
+                    </div>
+                    <p className='w-full text-center'>SIGN UP WITH GOOGLE</p>
+                  </button>
+                </div>            
+                <p className='text-white font-bold mb-2 invisible md:visible '>Slack is free to try for as long as you’d like</p>
             </div>
-            <div>
-              <video className=""  height="400" src={video1} loop muted autoPlay={true}></video>
-            </div>
+            <div className=' md:w-2/6'>
+              <video className="md:max-w-2xl overflow-visible"  height="400" src={video1} loop muted autoPlay={true}></video>
+            </div>                            
           </div>
-          
+        </section>
+        <section className='section'>
+          <div>
+
+          </div>
         </section>
     </div>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,36 @@
 import React from 'react'
 import logo from '../media/logo-white.svg'
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+import { AiOutlineClose, AiOutlineMenu, AiOutlineDown } from 'react-icons/ai'
 
 function Navbar() {
   return (
     <div className='container mx-auto text-white flex justify-between items-center h-24'>
-        <a href='/'><img src={logo} /></a>
-        <div className='flex'>
-            <a className='p-4 hover:underline' href='/'>Product</a>
+      <div className='flex items-center'>
+        <a className='visible' href='/'><img src={logo} alt="" /></a>
+        <div className='flex invisible font-bold xl:visible'>
+            <a className='p-4 flex items-center' href='/'>Product<AiOutlineDown className='mx-1 h-3 '/></a>
             <a className='p-4 hover:underline' href='/solutions'>Solutions</a>
             <a className='p-4 hover:underline' href='/'>Enterprise</a>
             <a className='p-4 hover:underline' href='/'>Resources</a>
             <a className='p-4 hover:underline' href='/'>Pricing</a>
         </div>
-        <div>
-            <a href='/'>Sign in</a>
-            <button>TALK TO SALES</button>
-            <button>TRY FOR FREE</button>
-        </div>
-        <div className='hidden'>
-          <AiOutlineMenu size={20} />
-        </div>
-        <div className='fixed left-0 top-0 hidden'>
-            <a className='p-4' href='/'>Product</a>
-            <a className='p-4' href='/solutions'>Solutions</a>
-            <a className='p-4' href='/'>Enterprise</a>
-            <a className='p-4' href='/'>Resources</a>
-            <a className='p-4' href='/'>Pricing</a>
-        </div>
+      </div>
+        
+      <div className='flex items-center invisible xl:visible'>
+          <a href='/' className='mx-2 font-bold'>Sign in</a>
+          <button className='mx-2 p-3 px-4 my-2 text-sm rounded border border-white text-white font-bold' >TALK TO SALES</button>
+          <button className='mx-2 p-3 px-4 my-2 text-sm rounded bg-white text-purple font-bold'>TRY FOR FREE</button>
+      </div>
+      <div className='visible xl:invisible'>
+        <AiOutlineMenu size={20} />
+      </div>
+      <div className='fixed left-0 top-0 hidden'>
+          <a className='p-4' href='/'>Product</a>
+          <a className='p-4' href='/solutions'>Solutions</a>
+          <a className='p-4' href='/'>Enterprise</a>
+          <a className='p-4' href='/'>Resources</a>
+          <a className='p-4' href='/'>Pricing</a>
+      </div>
     </div>
   )
 }
