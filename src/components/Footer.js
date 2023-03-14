ca141160be0261399fa8613f14fa96dd230b09d7
Caroly <93351229+cawx@users.noreply.github.com>
2023-03-14 12:40:20 +0200

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import logo from '../media/logo.svg'
-import { AiOutlineTwitter, AiFillFacebook, AiFillYoutube, AiFillLinkedin } from 'react-icons/ai'
+import { AiOutlineTwitter, AiFillFacebook, AiFillYoutube, AiFillLinkedin, AiOutlineCloudDownload } from 'react-icons/ai'
+import { TfiWorld, TfiAngleDown } from 'react-icons/tfi'
 
 function Footer() {
   return (
     <div>
-        <div className='container mx-auto py-20'>
+        <div className='container mx-auto pt-20 pb-10'>
             <div className='lg:flex lg:mx-24'>
                 <div className=''>
                   <img src={logo} className="visible self-start mb-10 lg:mr-36" />
@@ -51,26 +52,28 @@ function Footer() {
                     </div> 
                 </div>
             </div>
-            <div>
-                <div className='w-full border-b-2 my-5 border-black opacity-10'></div>
-                <div>
-                    <div>
-                        <a href='/'>Status</a>
-                        <a href='/'>Privacy</a>
-                        <a href='/'>Terms</a>
-                        <a href='/'>Cookie Preferences</a>
-                        <a href='/'>Contact Us</a>
-                        <a href='/'>Change Region</a>     
+            <div className='w-full border-b-2 my-5 border-black opacity-10'></div>
+            <div>  
+                <div className='flex justify-between items-center flex-wrap w-full'>
+                    <div className='flex flex-col md:flex-row font-black tracking-wide text-sm md:space-x-5'>
+                        <a href='/' className='py-2 hover:text-bluelink'>Status</a>
+                        <a href='/' className='py-2 hover:text-bluelink'>Privacy</a>
+                        <a href='/' className='py-2 hover:text-bluelink'>Terms</a>
+                        <a href='/' className='py-2 hover:text-bluelink'>Cookie Preferences</a>
+                        <a href='/' className='py-2 hover:text-bluelink'>Contact Us</a>
+                        <a href='/' className='py-2 hover:text-bluelink flex items-center'><TfiWorld className='mr-1 text-lg'/>Change Region<TfiAngleDown className='ml-1'/></a>     
                     </div>
-                    <div>
-                        <a href='/'>Download Slack</a>
-                        <AiOutlineTwitter />
-                        <AiFillFacebook />
-                        <AiFillYoutube />
-                        <AiFillLinkedin />  
-                    </div>
-                    <p>©2023 Slack Technologies, LLC, a Salesforce company. All rights reserved. Various trademarks held by their respective owners.</p>
+                    <div className='flex flex-col w-full lg:items-center lg:flex-row lg:w-auto '>
+                        <a href='/' className='flex items-center text-bluelink font-bold my-2 whitespace-nowrap'><AiOutlineCloudDownload className='text-2xl mr-2' />Download Slack</a>
+                        <div className='flex justify-between md:justify-start w-full lg:mx-3'>
+                            <AiOutlineTwitter className='text-xl md:mx-1 hover:text-bluelink hover:cursor-pointer'/>
+                            <AiFillFacebook className='text-xl md:mx-1 hover:text-bluelink hover:cursor-pointer'/>
+                            <AiFillYoutube className='text-xl md:mx-1 hover:text-bluelink hover:cursor-pointer'/>
+                            <AiFillLinkedin className='text-xl md:mx-1 hover:text-bluelink hover:cursor-pointer'/>  
+                        </div>
+                    </div>                   
                 </div>
+                <p className='text-sm mt-5'>©2023 Slack Technologies, LLC, a Salesforce company. All rights reserved. Various trademarks held by their respective owners.</p>
             </div>
         </div>
     </div>
